eed412e960879b2bd6eacfa4233a77fefa142171
pwcodigo <pwcodigo@gmail.com>
2020-12-23 01:35:00 -0300

    feat: Criado rota index para login e e tb local para proteger as rotas

--- app/routes/api.php
+++ app/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function (){
+
+});
+
+Route::post('login',[UserController::class, 'index']);
